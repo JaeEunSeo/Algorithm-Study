d33f1b8b422aab19e6d6b5535537fe54f9474d6b
JaeEunSeo <itws_009@naver.com>
2023-06-29 00:58:52 +0900

    ADD 9093

--- jaeeun/week 1/10828 스택.py
+++ jaeeun/week 1/10828 스택.py
@@ -4,24 +4,25 @@
 #empty -> bool로 판단
 #top -> 
 import sys
-input = sys.stdin.readline
-n = int(input())
+sys.stdin = open('/Users/jaeeun/Algorithm-Study/jaeeun/week 1/10828tc1.txt', 'r')
+n = int(sys.stdin.readline())
 
 stack = []
 
 for i in range(n):
-    command = input().split()
+    command = sys.stdin.readline().split()
     if command[0] == 'push':
         stack.append(command[1])
     elif command[0] == 'pop':
         if not stack :
             print("-1")
         else:
+            print(stack[len(stack)-1])
             stack.pop()
     elif command[0] == 'size':
         print(len(stack))
     elif command[0] == 'empty':
-        if (stack.empty()): print("1")
+        if not stack: print("1")
         else:print("0")
     elif command[0] == 'top':
         if not stack : print("-1")
