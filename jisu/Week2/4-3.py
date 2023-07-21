5f8f69fd100555d64a25fba79d4be7369194fd8a
Jisu <jsomnium@naver.com>
2023-07-21 20:53:43 +0900

    [CORRECT] 오타 수정

--- jisu/Week2/4-3.py
+++ jisu/Week2/4-3.py
@@ -1,7 +1,7 @@
 # 현재 나이트의 위치 입력받기
 input_data = input()
 row=int(input_data[1])
-column=int(ord(input_data[0]0))-int(ord('a'))+1
+column=int(ord(input_data[0])) - int(ord('a')) + 1
 
 # 나이트가 이동할 수 있는 8가지 방향 정의
 steps=[
