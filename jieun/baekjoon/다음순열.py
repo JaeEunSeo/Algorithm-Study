bfc71d07538456cdf22157c5dae14caf4a552e61
yumzen <yeom.ji.eun35@gmail.com>
2023-09-04 11:20:14 +0900

    [ADD] 10972번 다음 순열 문제 풀이

--- jieun/baekjoon/다음순열.py
+++ jieun/baekjoon/다음순열.py
@@ -2,7 +2,9 @@ n = int(input())
 
 num = list(map(int, input().split()))
 find = False
+
 for i in range(n-1, 0, -1):
+
     if num[i-1] < num[i]: #뒷값이 앞값보다 큰 경우
         for j in range(n-1, 0, -1):
             if num[i-1] < num[j]:
@@ -10,6 +12,7 @@ for i in range(n-1, 0, -1):
                 num = num[:i] + sorted(num[i:])
                 find = True
                 break
+
     if find:
         print(" ".join(map(str, num)))
         break
